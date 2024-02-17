816a444ac40536ad821f932163f48e32dfadda7d
Cameren Dessert <phazedverse@gmail.com>
2024-02-16 21:43:26 -0700

    Update index.js
    Correct index for simplified importing

--- src/renderer/src/routes/advRoutes/index.js
+++ src/renderer/src/routes/advRoutes/index.js
@@ -1,5 +1,5 @@
 // @create-index
 
-export { default as City, loader as cityLoader } from './City.jsx';
-export { default as viewCity } from './ViewCountry.jsx';
+export { default as ViewCity } from './ViewCity.jsx';
+export { default as ViewCountry } from './ViewCountry.jsx';
 
