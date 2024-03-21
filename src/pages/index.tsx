a0d2d8fe563b155e6c5d0a4b864b0fb4645768d2
Cameren Dessert <phazedverse@gmail.com>
2024-03-21 02:41:59 -0600

    fix: :bug: Corrected routes to settings page.
    Corrected routes to settings page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-export {default as HomePage} from "./HomePage";
-export {default as Settings} from "./Settings";
-export {default as ErrorPage} from "./ErrorPage";
-export {default as Overview} from "./Overview";
-export {default as Tags} from "./Tags";
+export { default as ErrorPage } from './ErrorPage';
+export { default as HomePage } from './HomePage';
+export { default as Overview } from './Overview';
+export { default as Settings } from './Settings/Settings';
+export { default as Tags } from './Tags';
