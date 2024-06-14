db89789472f324410e33a0ceaa9b6cda55343ca0
Cameren Dessert <phazedverse@gmail.com>
2024-06-14 17:53:03 -0600

    refactor: :recycle: Implement basic city view with proper data loading

--- src/pages/ViewingPages/city.tsx
+++ src/pages/ViewingPages/city.tsx
@@ -1,19 +1,30 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import { useMemo } from 'react';
 import { IconContext } from 'react-icons';
 import { useParams } from 'react-router-dom';
+import { getDataFromStore } from '../../db/interactions';
 
 function CityView() {
   let cityId = useParams();
+  const [city, setCity] = useState<TLCity>();
+
+  useEffect(() => {
+    if (cityId !== undefined) {
+      getDataFromStore('cities', cityId._id).then((data) => {
+        setCity(data as TLCity);
+      })
+    }
+  }, []);
 
   const IconStyles = useMemo(() => ({ size: '1.5rem' }), []);
 
   return (
     <Container className="Settings">
       <IconContext.Provider value={IconStyles}>
-        <h3>{}</h3>
-        <div className="contentSubBody">{String(cityId)}</div>
+        <h3>{city?.name}</h3>
+        <div className="contentSubBody"></div>
       </IconContext.Provider>
     </Container>
   );
