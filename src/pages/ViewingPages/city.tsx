ee3b859557b7d0a0885f6aed26be7307c0241159
Cameren Dessert <phazedverse@gmail.com>
2024-06-15 03:48:41 -0600

    refactor: :sparkles: provide basic page layout for city view

--- src/pages/ViewingPages/city.tsx
+++ src/pages/ViewingPages/city.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Container } from '@mui/material';
-import { useMemo } from 'react';
 import { IconContext } from 'react-icons';
 import { useParams } from 'react-router-dom';
 import { getDataFromStore } from '../../db/interactions';
+
+import './city.css'
 
 function CityView() {
   let cityId = useParams();
@@ -19,12 +20,74 @@ function CityView() {
   }, []);
 
   const IconStyles = useMemo(() => ({ size: '1.5rem' }), []);
+  console.log(JSON.stringify(city));
 
   return (
     <Container className="Settings">
       <IconContext.Provider value={IconStyles}>
-        <h3>{city?.name}</h3>
-        <div className="contentSubBody"></div>
+        <div className="contentSubBody">
+        <div className="flex-container">
+          <section className="citySection">
+            <div className="container">
+              <div className="grid-container">
+                <div className="content">
+                  <div className="text-content">
+                    <h1 className="title">{city?.name}</h1>
+                    <p className="location">{city?.country.name}</p>
+                    <p className="population">Population: {city?.population}</p>
+                  </div>
+                </div>
+                <div className="image-container" dangerouslySetInnerHTML={{ __html: city?.coaSVG ?? ''}} />
+              </div>
+            </div>
+          </section>
+          <section className="citySection">
+            <div className="container grid-layout">
+              <div className="text-content">
+                <h2 className="title">Description</h2>
+                <p className="description">
+                  {city?.description}
+                </p>
+                <div className="features-culture">
+                  <div className="features">
+                    <h3 className="subtitle">Features</h3>
+                    <ul className="list">
+                      {city?.features.map((feature) => (
+                        <li key={feature}>{feature}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div className="culture">
+                    <h3 className="subtitle">Culture</h3>
+                    <p>get culture information and render it here:</p>
+                  </div>
+                </div>
+              </div>
+              <div className="text-content">
+                <h2 className="title">&nbsp;</h2>
+                <p className="description">
+                  {city?.description}
+                </p>
+                <div className="features-culture">
+                  <div className="features">
+                    <h3 className="subtitle">Tags</h3>
+                    <ul className="list">
+                      {city?.features.map((feature) => (
+                        <li key={feature}>{feature}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div className="culture">
+                    <h3 className="subtitle">Map Link</h3>
+                    <p>render Map link here</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        </div>
       </IconContext.Provider>
     </Container>
   );
