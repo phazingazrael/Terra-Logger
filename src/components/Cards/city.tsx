36a98b0e7ef9a2b00c9bf1ed7fc18bfe2e259405
Cameren Dessert <phazedverse@gmail.com>
2025-01-24 17:12:44 -0700

    Correct React errors of having multiple children with same id

--- src/components/Cards/city.tsx
+++ src/components/Cards/city.tsx
@@ -1,90 +1,100 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Chip, Typography } from '@mui/material';
-import PropTypes from 'prop-types';
-import LinesEllipsis from 'react-lines-ellipsis';
-import { Link, useOutletContext } from 'react-router-dom';
+import type { Theme } from "@mui/material";
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Chip,
+	Typography,
+} from "@mui/material";
+import PropTypes from "prop-types";
+import Ellipsis from "../Util/ellipsis";
+import { Link, useOutletContext } from "react-router-dom";
 
-import { rgbToRgba, LazyLoadedSVG } from '../Util';
+import { rgbToRgba, LazyLoadedSVG } from "../Util";
 
-import './cards.css';
+import "./cards.css";
 
 function CityCard(props: Readonly<TLCity>) {
-  const city = props;
-  const theme: any = useOutletContext();
+	const city = props;
+	const theme: Theme = useOutletContext();
 
-  let themeColor = null;
+	let themeColor = null;
 
-  if (theme.palette.mode === 'dark') {
-    themeColor = theme.palette.primary.dark;
-  } else if (theme.palette.mode === 'light') {
-    themeColor = theme.palette.primary.light;
-  }
+	if (theme.palette.mode === "dark") {
+		themeColor = theme.palette.primary.dark;
+	} else if (theme.palette.mode === "light") {
+		themeColor = theme.palette.primary.light;
+	}
 
-  const ImageAlt = '';
-  return (
-    <Card>
-      <CardMedia
-        sx={{ backgroundColor: theme ? rgbToRgba(themeColor as string, 0.5 as number) : '' }}
-        title={ImageAlt}
-      >
-        {/* <LazyLoadedSVG {...(city.coa as TLCoA)} /> */}
-        {city.coaSVG === '' || city.coaSVG ==="<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n<meta charset=\"utf-8\">\n<title>Error</title>\n</head>\n<body>\n<pre>Internal Server Error</pre>\n</body>\n</html>\n" ? <LazyLoadedSVG {...(city.coa as TLCoA)} /> :<div className="svg-container CoA" dangerouslySetInnerHTML={{__html: city.coaSVG}}></div> }
-      </CardMedia>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {city.name}
-        </Typography>
-        {city.description !== '' && (
-        <LinesEllipsis
-        text={city.description}
-        maxLine="2"
-        ellipsis="..."
-        trimRight
-        basedOn="letters"
-        id={'city.id-' + city._id}
-        className="description"
-        component="p"
-      />
-        )
-        }
-
-      </CardContent>
-      <CardActions className="tile-info">
-        <div className="tile-category">
-          Tags:
-          <br />
-          {city.tags.map((tag) => (
-            <Chip size="small" key={city._id} label={tag.Name} />
-          ))}
-        </div>
-        <Link to={'/view_city/' + city._id}>
-          <Button className="tile-button" color="secondary" variant="contained">
-            View
-          </Button>
-        </Link>
-      </CardActions>
-    </Card>
-  );
+	const ImageAlt = "";
+	return (
+		<Card>
+			<CardMedia
+				sx={{
+					backgroundColor: theme
+						? rgbToRgba(themeColor as string, 0.5 as number)
+						: "",
+				}}
+				title={ImageAlt}
+			>
+				{/* <LazyLoadedSVG {...(city.coa as TLCoA)} /> */}
+				{city.coaSVG === "" ||
+				city.coaSVG ===
+					'<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8">\n<title>Error</title>\n</head>\n<body>\n<pre>Internal Server Error</pre>\n</body>\n</html>\n' ? (
+					<LazyLoadedSVG {...(city.coa as TLCoA)} />
+				) : (
+					<div
+						className="svg-container CoA"
+						// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+						dangerouslySetInnerHTML={{ __html: city.coaSVG }}
+					/>
+				)}
+			</CardMedia>
+			<CardContent>
+				<Typography gutterBottom variant="h5" component="div">
+					{city.name}
+				</Typography>
+				{city.description !== "" && <Ellipsis>{city.description}</Ellipsis>}
+			</CardContent>
+			<CardActions className="tile-info">
+				<div className="tile-category">
+					Tags:
+					<br />
+					{city.tags.map((tag) => (
+						<Chip size="small" key={`tag-${tag._id}`} label={tag.Name} />
+					))}
+				</div>
+				<Link to={`/view_city/${city._id}`} key={`city-${city._id}`}>
+					<Button className="tile-button" color="secondary" variant="contained">
+						View
+					</Button>
+				</Link>
+			</CardActions>
+		</Card>
+	);
 }
 
 CityCard.propTypes = {
-  city: PropTypes.shape({
-    _id: PropTypes.string,
-    name: PropTypes.string,
-    coa: PropTypes.object,
-    description: PropTypes.string,
-    tags: PropTypes.array,
-  }),
-  theme: PropTypes.shape({
-    palette: PropTypes.shape({
-      mode: PropTypes.string,
-      primary: PropTypes.shape({
-        dark: PropTypes.string,
-        light: PropTypes.string,
-      }),
-      text: PropTypes.shape({
-        primary: PropTypes.string,
-      }),
-    }),
-  }),
+	city: PropTypes.shape({
+		_id: PropTypes.string,
+		name: PropTypes.string,
+		coa: PropTypes.object,
+		description: PropTypes.string,
+		tags: PropTypes.array,
+	}),
+	theme: PropTypes.shape({
+		palette: PropTypes.shape({
+			mode: PropTypes.string,
+			primary: PropTypes.shape({
+				dark: PropTypes.string,
+				light: PropTypes.string,
+			}),
+			text: PropTypes.shape({
+				primary: PropTypes.string,
+			}),
+		}),
+	}),
 };
 export default CityCard;
