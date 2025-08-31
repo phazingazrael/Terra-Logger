c4f58b468dbc6b2096127408d37e0a32750b8f91
Cameren Dessert <phazedverse@gmail.com>
2025-08-31 10:11:54 -0600

    perf: :zap: Optimize code for performance on database creation/loading
    Optimize code for performance on database creation/loading

--- src/db/database.tsx
+++ src/db/database.tsx
@@ -1,5 +1,16 @@
 import { openDB } from "idb";
 
+const stores = [
+	"cities",
+	"countries",
+	"cultures",
+	"notes",
+	"npcs",
+	"religions",
+	"nameBases",
+	"tags",
+];
+
 export const initDatabase = async () => {
 	const db = await openDB("TerraLogger-Maps", 1, {
 		upgrade(db) {
@@ -7,24 +18,17 @@ export const initDatabase = async () => {
 			const mapStore = db.createObjectStore("maps", { keyPath: "id" });
 			mapStore.createIndex("mapIdIndex", "mapId");
 
-			// Create individual stores for each entity\
-			for (const entity of [
-				"cities",
-				"countries",
-				"cultures",
-				"notes",
-				"npcs",
-				"religions",
-				"nameBases",
-        "tags"
-			]) {
-				const store = db.createObjectStore(entity, { keyPath: "_id" });
-				store.createIndex("mapIdIndex", "mapId");
-			}
+			// Create individual stores for each
+			const promises = stores.map((Store) => {
+				const store = db.createObjectStore(Store, { keyPath: "_id" });
+				return store.createIndex("mapIdIndex", "mapId");
+			});
 
-			// Create settings store
-			db.createObjectStore("appSettings", { keyPath: "id" });
-			// apply defaults
+			// Return a promise that resolves when all index creations are complete
+			return Promise.all(promises).then(() => {
+				// Create settings store
+				db.createObjectStore("appSettings", { keyPath: "id" });
+			});
 		},
 	});
 
